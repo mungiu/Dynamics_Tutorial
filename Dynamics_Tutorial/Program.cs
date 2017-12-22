e1ac850f9422fc28b30493cc3e09edc7f50630f8
Andrei Mungiu <andrei_mungiu@yahoo.com>
2017-12-22 18:38:42 +0100

    Tutorial finalized

--- Dynamics_Tutorial/Program.cs
+++ Dynamics_Tutorial/Program.cs
@@ -10,6 +10,37 @@ namespace Dynamics_Tutorial
     {
         static void Main(string[] args)
         {
+            object obj = "Andrei";
+            obj.GetHashCode();
+
+            ////////////////////////// using REFLECTION ///////////////
+
+            ////same as above (messy and dificult)
+            //var methodInfo = obj.GetType().GetMethod("GetHashCode");
+            //methodInfo.Invoke(null, null);
+
+            ////////////////////////// using DYNAMICS /////////////////
+
+            //object excelObject = "excelInfo";
+            //excelObject.Optimize();           ////won't work, method not existent
+
+            ////same as reflection but cleaner code
+            //dynamic excelObject = "mosh";       
+            //excelObject.Optimize();
+
+            //NOTE: "name" is easelly converted from string to int since its "dynamic"
+            dynamic name = "Andrei";
+            name = 10;
+            name++;     //NOTE: won;t work if name is string
+
+            Console.WriteLine(name);
+
+            dynamic a = 10;
+            dynamic b = 5;
+            var c = a + b;  //NOTE: var becomes "dynamic"
+            c = "asd";
+
+            Console.WriteLine(c);
         }
     }
 }
